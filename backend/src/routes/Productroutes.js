8647394a098faa86bbdf905fd92ab0d32724c8e8
Kavivarman-Developer <kavivarman6722@gmail.com>
2026-07-23 12:23:21 +0530

    update Prodectcontroller.js file

--- backend/src/routes/Productroutes.js
+++ backend/src/routes/Productroutes.js
@@ -1,16 +1,16 @@
 import express from "express";
 const router = express.Router();
-import { getProducts, getAllProducts, createProduct, updateProduct, deleteProduct } from "../controllers/ProductController.js";
+import { getProducts, getAllProducts, createProduct, updateProduct, deleteProduct } from "../controllers/Productcontroller.js";
 
 import { verifyToken } from "../middleware/authMiddleware.js";
 
-// PUBLIC — CustomerHome fetch பண்ண (token தேவையில்லை)
+// PUBLIC — CustomerHome fetch 
 router.get("/", getProducts);
 
 // ADMIN — CRM Products.jsx
-router.get("/all",       verifyToken, getAllProducts);
-router.post("/",         verifyToken, createProduct);
-router.put("/:id",       verifyToken, updateProduct);
-router.delete("/:id",    verifyToken, deleteProduct);
+router.get("/all", verifyToken, getAllProducts);
+router.post("/", verifyToken, createProduct);
+router.put("/:id", verifyToken, updateProduct);
+router.delete("/:id", verifyToken, deleteProduct);
 
 export default router;
